b390bec351b90bcdc736714070dcd322bc9ff297
Frempong <samankwaa.frempong@gmail.com>
2023-03-02 22:03:01 +0000

    Update Apps.js to add button functionalities

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
+const initialState = { total: 0, next: '', operation: '' };
 const Calculator = () => {
-  const [inputValue, setInputValue] = useState('0');
+  const [state, setState] = useState(initialState);
 
   const handleNumberClick = (e) => {
-    const clickedNumber = parseFloat(e.target.textContent);
-    setInputValue((prevInputValue) => prevInputValue + clickedNumber);
-  };
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
+    const response = calculate(state, e.target.textContent);
+    setState(response);
   };
 
   return (
-
     <div className="calc-box">
       <div className="calc-div">
-        <input id="answer" className="answer" value={inputValue} onChange={handleChange} />
-        <button type="button" id="reset" className="special">AC</button>
-        <button type="button" id="plus-or-minus" className="special">+/-</button>
-        <button type="button" id="percent" className="special">%</button>
-        <button type="button" id="divide-by" className="operand">÷</button>
+        <div id="answer" className="answer">
+          {state.total}
+          {state.operation}
+          {state.next}
+        </div>
+        <button type="button" onClick={handleNumberClick} id="reset" className="special">AC</button>
+        <button type="button" onClick={handleNumberClick} id="plus-or-minus" className="special">+/-</button>
+        <button type="button" onClick={handleNumberClick} id="percent" className="special">%</button>
+        <button type="button" onClick={handleNumberClick} id="divide-by" className="operand">÷</button>
         <button type="button" onClick={handleNumberClick} id="7" className="num">7</button>
         <button type="button" onClick={handleNumberClick} id="8" className="num">8</button>
         <button type="button" onClick={handleNumberClick} id="9" className="num">9</button>
-        <button type="button" id="times" className="operand">x</button>
+        <button type="button" onClick={handleNumberClick} id="times" className="operand">x</button>
         <button type="button" onClick={handleNumberClick} id="4" className="num">4</button>
         <button type="button" onClick={handleNumberClick} id="5" className="num">5</button>
         <button type="button" onClick={handleNumberClick} id="6" className="num">6</button>
-        <button type="button" id="minus" className="operand">-</button>
+        <button type="button" onClick={handleNumberClick} id="minus" className="operand">-</button>
         <button type="button" onClick={handleNumberClick} id="1" className="num">1</button>
         <button type="button" onClick={handleNumberClick} id="2" className="num">2</button>
         <button type="button" onClick={handleNumberClick} id="3" className="num">3</button>
-        <button type="button" id="plus" className="operand">+</button>
+        <button type="button" onClick={handleNumberClick} id="plus" className="operand">+</button>
         <button type="button" onClick={handleNumberClick} id="zero" className="num">0</button>
-        <button type="button" id="dot" className="special">.</button>
-        <button type="button" id="equal-to" className="operand">=</button>
+        <button type="button" onClick={handleNumberClick} id="dot" className="special">.</button>
+        <button type="button" onClick={handleNumberClick} id="equal-to" className="operand">=</button>
       </div>
     </div>
   );
