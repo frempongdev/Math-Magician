16d2494103102e47dc098a896c9d06bb44238ef5
Frempong <samankwaa.frempong@gmail.com>
2023-03-09 16:01:40 +0000

    Update calculate.test.js to pass test

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -2,15 +2,15 @@
 import calculate from '../logic/calculate.js';
 
 describe('calculate', () => {
-  test('should set the next value to 0 when given the "0" button', () => {
-    const state = { total: 0, next: null, operation: null };
-    const result = calculate(state, '0');
-    expect(result).toEqual({ total: null, next: '0', operation: null });
-  });
-
   test('should add a decimal point to the next value when given the "." button', () => {
     const state = { total: 0, next: '3', operation: null };
     const result = calculate(state, '.');
     expect(result).toEqual({ total: 0, next: '3.', operation: null });
+  });
+
+  it('should return the initial state when given the AC button', () => {
+    const initialState = { total: 0, next: null, operation: null };
+    const result = calculate({}, 'AC');
+    expect(result).toEqual(initialState);
   });
 });
